5c0378429832c3e559e1d1db9ef6dc6305d1cac6
Mark Tinderholt <markti@outlook.com>
2018-07-12 07:24:35 -0400

    working on android

---
+++ App2/App2.Android/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace App2.Droid
+{
+    [Activity(Label = "App2", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            ZXing.Net.Mobile.Forms.Android.Platform.Init();
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            global::ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
+
---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            this.barcodeImage.BarcodeOptions = new ZXing.Common.EncodingOptions()
+            {
+                Height = 300,
+                Width = 300
+            };
+            this.barcodeImage.BarcodeValue = Guid.NewGuid().ToString();
+        }
+    }
+}
